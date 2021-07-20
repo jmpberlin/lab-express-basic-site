2e46a3b7fac0164bdf9960a2ea205d0fe22ce4f0
Hannes Polte <hannes.polte@gmx.de>
2021-07-20 20:42:26 +0200

    added all view-pages - basic connections work!

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require ('express');
+const app = express();
+
+
+// HOME ROUTE
+app.get('/home', (request, response, next) => {
+
+
+    response.sendFile(__dirname + '/views/index.html');
+  
+  });
+
+
+  // ABOUT ROUTE
+app.get('/about', (request, response, next) => {
+
+
+    response.sendFile(__dirname + '/views/about.html');
+  
+  });
+
+
+  // WORKS ROUTE
+app.get('/works', (request, response, next) => {
+
+
+    response.sendFile(__dirname + '/views/works.html');
+  
+  });
+
+
+app.listen(3000, () => console.log('My first app listening on port 3000! '));
